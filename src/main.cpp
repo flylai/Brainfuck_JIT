56423031627a016263888582bf6860ca2266db0c
LKM <cnming2@qq.com>
2021-06-08 10:19:25 +0800

    feat: add options

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,21 @@
 #include <utility>
 #include <vector>
 
-int main(int argc, char *argv[]) {
-    std::ifstream in(argv[1], std::ios::in);
-    std::string code((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+//
+bool OPTION_JIT = false;
+bool OPTION_OPT = false;
+std::vector<Instruction *> instructions;
+//
+void showHelp() {
+    std::string str = "Usage:\n";
+    str += "    branfuck_jit [options] [src file]\n";
+    str += "where options include:\n";
+    str += "    -jit enable jit(interpreter is default)\n";
+    str += "    -opt enable optimizer";
+    std::cout << str;
+}
 
-    std::vector<Instruction *> instructions;
+void compile(const std::string &code) {
     std::stack<unsigned int> loop_stack;
 
     for (auto c : code) {
@@ -50,23 +60,44 @@ int main(int argc, char *argv[]) {
                 break;
         }
     }
+}
 
-    BrainfuckVM vm;
-
-    {
-        Optimizer optimizer;
-        optimizer.instructions = std::move(instructions);
-        optimizer.optimize();
-        //        vm.instructions = std::move(optimizer.optimized);
-        JIT jit;
-        jit.instructions = std::move(optimizer.optimized);
-        jit.compile();
-        jit.print();
-        jit.executeMem(jit.allocMem(30000));
+int main(int argc, char *argv[]) {
+    for (int i = 1; i < argc - 1; i++) {
+        std::string option = std::string(argv[i]);
+        if (option == "-jit") OPTION_JIT = true;
+        else if (option == "-opt")
+            OPTION_OPT = true;
+        else {
+            std::cerr << "Unknown option `" << option << "`\n";
+            showHelp();
+            exit(-1);
+        }
     }
 
 
-    //    vm.run();
+    std::ifstream in(argv[argc - 1], std::ios::in);
+    std::string code((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+
+    compile(code);
+
+    if (OPTION_OPT) {
+        Optimizer optimizer;
+        optimizer.instructions = std::move(instructions);
+        optimizer.optimize();
+        instructions = std::move(optimizer.optimized);
+    }
+
+    if (OPTION_JIT) {
+        JIT jit;
+        jit.instructions = std::move(instructions);
+        jit.compile();
+        jit.executeMem(jit.allocMem(30000));
+    } else {
+        BrainfuckVM vm;
+        vm.instructions = std::move(instructions);
+        vm.run();
+    }
 
     return 0;
 }
